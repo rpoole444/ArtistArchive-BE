30ac55904ca44d4d3f116c7e11fe9dcf86594b42
Reid Poole <poole.reid@gmail.com>
2023-08-05 00:15:37 -0600

    Edit Seeds

--- seeds/artists_favorites_seed.js
+++ seeds/artists_favorites_seed.js
@@ -4,7 +4,7 @@ exports.seed = function(knex) {
   return knex("artists")
     .del()
     .then(() => {
-      return knex.raw("ALTER SEQUENCE artists_id_seq RESTART WITH 1");
+      return knex.raw("ALTER SEQUENCE artists_pgid_seq RESTART WITH 1");
     })
     .then(() => {
       return knex("artists").insert(artistsData);
@@ -13,7 +13,7 @@ exports.seed = function(knex) {
       return knex("favorites")
         .del()
         .then(() => {
-          return knex.raw("ALTER SEQUENCE favorites_id_seq RESTART WITH 1");
+          return knex.raw("ALTER SEQUENCE favorites_pgid_seq RESTART WITH 1");
         })
     })
     
