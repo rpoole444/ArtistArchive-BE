a10e76222a62c28cff09176993548771d6abe61b
Reid Poole <poole.reid@gmail.com>
2023-08-05 00:32:17 -0600

    Comment out Crud Methods attached to queries *Needs debugging

--- server.js
+++ server.js
@@ -23,93 +23,93 @@ app.get("/", (request, response) => {
 });
 
 app.get("/api/v1/artists", (request, response) => {
-  // const { artists } = app.locals;
-  // response.json({ artists });
-  queries
-    .getAllArtists()
-    .then((data) => response.status(200).json(data))
-    .catch((error) => response.status(500).json({ error }))
+  const { artists } = app.locals;
+  response.json({ artists });
+  // queries
+  //   .getAllArtists()
+  //   .then((data) => response.status(200).json(data))
+  //   .catch((error) => response.status(500).json({ error }))
 });
 
 app.get("/api/v1/artists/:id", (request, response) => {
   const { id } = request.params;
 
   const artist = app.locals.artists.find((artist) => artist.id === id);
-  // if (!artist) {
-  //   return response.status(404).send("No id Matches");
-  // } else {
-  //   response.status(200).json(artist);
-  // }
-
-  queries
-  .getSingleArtist()
-  .then((artists) => {
-    if(artists.length) {
-      response.status(200).json(artists)
-    } else { 
-      response.status(404).json({
-        error: `no artist found with id of # ${request.params.id}`
-      })
-    }
-  })
-  .catch((error) => response.status(500).json({ error }))
-});
-
-app.get("/api/v1/favorites", (request, response) => {
-  queries
-    .getAllFavorites()
-    .then((data) => response.status(200).json(data))
-    .catch((error) => response.status(500).json({ error }));
-});
-
-app.delete("/api/v1/favorites/:id", (request, response) => {
-  queries
-  .removeBookFromFavorites(request).then((unfavorited) => {
-    if (unfavorited) {
-      return response.status(200).json({
-        message: `Artist with id number ${request.params.id} has been removed from favorites`,
-      });
-    } else {
-      response.status(404).json({
-        error: `could not find artist based on id ${request.params.id}`,
-      });
-    }
-  });
-});
-
-app.post("/api/v1/favorites", (request, response) => {
-  const favorite = request.body;
-  const {
-    id,
-    name,
-    image,
-    rank,
-    description,
-    genre,
-    video,
-    isFavorited,
-  } = favorite;
-  for (let requiredParameter of [
-    "id",
-    "name",
-    "image",
-    "rank",
-    "description",
-    "genre",
-    "video",
-    "isFavorited",
-  ]) {
-    if (!favorite[requiredParameter]) {
-      response.status(422).send({
-        error: `Expected format: {id:<String>, name: <String>, image:<String>, rank:<Number>, description: <String>, genre: <String>, video: <String>, isFavorited: <Boolean>}. You're missing a "${requiredParameter}" property.`,
-      });
-    }
+  if (!artist) {
+    return response.status(404).send("No id Matches");
+  } else {
+    response.status(200).json(artist);
   }
-  queries
-    .addArtistToFavorites(favorite)
-    .then((data) => response.status(201).json(data))
-    .catch((error) => response.status(500).json({ error }));
+
+  // queries
+  // .getSingleArtist()
+  // .then((artists) => {
+  //   if(artists.length) {
+  //     response.status(200).json(artists)
+  //   } else { 
+  //     response.status(404).json({
+  //       error: `no artist found with id of # ${request.params.id}`
+  //     })
+  //   }
+  // })
+  // .catch((error) => response.status(500).json({ error }))
 });
+
+// app.get("/api/v1/favorites", (request, response) => {
+//   queries
+//     .getAllFavorites()
+//     .then((data) => response.status(200).json(data))
+//     .catch((error) => response.status(500).json({ error }));
+// });
+
+// app.delete("/api/v1/favorites/:id", (request, response) => {
+//   queries
+//   .removeBookFromFavorites(request).then((unfavorited) => {
+//     if (unfavorited) {
+//       return response.status(200).json({
+//         message: `Artist with id number ${request.params.id} has been removed from favorites`,
+//       });
+//     } else {
+//       response.status(404).json({
+//         error: `could not find artist based on id ${request.params.id}`,
+//       });
+//     }
+//   });
+// });
+
+// app.post("/api/v1/favorites", (request, response) => {
+//   const favorite = request.body;
+//   const {
+//     id,
+//     name,
+//     image,
+//     rank,
+//     description,
+//     genre,
+//     video,
+//     isFavorited,
+//   } = favorite;
+//   for (let requiredParameter of [
+//     "id",
+//     "name",
+//     "image",
+//     "rank",
+//     "description",
+//     "genre",
+//     "video",
+//     "isFavorited",
+//   ]) {
+//     if (!favorite[requiredParameter]) {
+//       response.status(422).send({
+//         error: `Expected format: {id:<String>, name: <String>, image:<String>, rank:<Number>, description: <String>, genre: <String>, video: <String>, isFavorited: <Boolean>}. You're missing a "${requiredParameter}" property.`,
+//       });
+//     }
+//   }
+//   queries
+//     .addArtistToFavorites(favorite)
+//     .then((data) => response.status(201).json(data))
+//     .catch((error) => response.status(500).json({ error }));
+// });
 
 app.listen(app.get("port"), () => {
   console.log(
